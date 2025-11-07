b0c72e04bc7446a7de762681b7c8dc4000b6df78
Noah Christian Le Roy <noahleroy@gmail.com>
2025-11-08 00:36:46 +0100

    led idle timer 20 min

--- keyboards/bastardkb/charybdis/4x6/keymaps/noah/config.h
+++ keyboards/bastardkb/charybdis/4x6/keymaps/noah/config.h
@@ -86,4 +86,4 @@
 #define AUTO_MOUSE_TIME 1200 // milliseconds
 
 // --- RGB Matrix Idle Timeout ---
-#define RGB_MATRIX_IDLE_TIMEOUT_MIN 1 // minutes
+#define RGB_MATRIX_IDLE_TIMEOUT_MIN 20 // minutes
