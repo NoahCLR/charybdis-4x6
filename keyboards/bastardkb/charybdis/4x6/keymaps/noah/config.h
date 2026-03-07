221274a8cd7648ddbecf496790c46b6094cca20c
Noah Christian Le Roy <noahleroy@gmail.com>
2026-03-07 23:42:30 +0100

    num layer

--- keyboards/bastardkb/charybdis/4x6/keymaps/noah/config.h
+++ keyboards/bastardkb/charybdis/4x6/keymaps/noah/config.h
@@ -3,7 +3,7 @@
 // ─── Core / feature toggles ─────────────────────────────────────────────────
 #ifdef VIA_ENABLE
 #    undef DYNAMIC_KEYMAP_LAYER_COUNT
-#    define DYNAMIC_KEYMAP_LAYER_COUNT 4
+#    define DYNAMIC_KEYMAP_LAYER_COUNT 5
 #endif
 
 #ifndef __arm__
